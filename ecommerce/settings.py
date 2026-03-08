12c70bb5eb8877b920423b48777886e357cf3378
DaniLopAr <dj01lopeza@gmail.com>
2026-03-07 23:48:25 -0600

    text email

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -182,7 +182,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('BREVO_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('BREVO_SMTP_KEY')
