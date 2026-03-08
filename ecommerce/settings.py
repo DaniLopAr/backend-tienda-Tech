00dd4ed62af68924691cdaf86077b018ea80700e
DaniLopAr <dj01lopeza@gmail.com>
2026-03-07 22:41:27 -0600

    add brevo email

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -176,3 +176,13 @@ RESEND_API_KEY = config('RESEND_API_KEY')
 
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('BREVO_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('BREVO_SMTP_KEY')
+DEFAULT_FROM_EMAIL = os.environ.get('BREVO_EMAIL')
+
